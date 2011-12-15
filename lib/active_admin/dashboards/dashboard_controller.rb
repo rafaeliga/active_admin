32f0094ee6b5fd634b19c3c8983d974e3af74c3b
Rafael Iga <rafael.f.iga@gmail.com>
2011-12-15 00:19:40 -0200

    REAL: conditional :if for sections on Dashboard

--- lib/active_admin/dashboards/dashboard_controller.rb
+++ lib/active_admin/dashboards/dashboard_controller.rb
@@ -16,7 +16,19 @@ module ActiveAdmin
       end
 
       def find_sections
-        ActiveAdmin::Dashboards.sections_for_namespace(namespace)
+        sections = ActiveAdmin::Dashboards.sections_for_namespace(namespace)        
+        sections.select do |section|
+          if section.options.has_key?(:if)
+            symbol_or_proc = section.options[:if]
+            case symbol_or_proc
+            when Symbol, String then self.send(symbol_or_proc)
+            when Proc           then instance_exec(&symbol_or_proc)
+            else symbol_or_proc
+            end
+          else
+            true
+          end
+        end
       end
 
       def namespace
